9001d2b9288b758f499d19a300bd1d4a2c83ca81
User <User@DESKTOP-9T5HOH1>
2023-03-30 10:19:27 +0300

    Changes 1

---
+++ SearchEngine/Search.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Search.aspx.cs" Inherits="SearchEngine.Search" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="searchTerm" runat="server" ></asp:TextBox>
+            <asp:Button ID="searchBtn" runat="server" OnClick="DoSearch" Text="Search..."/>
+        </div>
+        <br />
+        <div id="resultsSearch" runat="server">
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ SearchEngine/Search.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SearchEngine
+{
+    public partial class Search : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+
+            }
+        }
+        public static string HtmlEncode(string text, int fromN = 127)
+        {
+            if (text == null)
+            {
+                return null;
+            }
+            char[] chars = HttpUtility.HtmlEncode(text).ToCharArray();
+            StringBuilder result = new StringBuilder(text.Length + (int)(text.Length * 0.1));
+
+            foreach (char c in chars)
+            {
+                int value = Convert.ToInt32(c);
+                if (value > fromN)
+                    result.AppendFormat("&#{0};", value);
+                else
+                    result.Append(c);
+            }
+
+            return result.ToString();
+        }
+        public string searchOneKeyword(string keyword)
+        {
+            string searchTerm = HtmlEncode(keyword);
+            string directoryPath = Server.MapPath("~/HTML");
+            string result = "";
+            IEnumerable<string> htmlFiles = Directory.GetFiles(directoryPath, "*.html", SearchOption.AllDirectories);
+
+            foreach (string htmlFile in htmlFiles)
+            {
+                int lineNumber = 1;
+                foreach (string line in File.ReadLines(htmlFile))
+                {
+                    if (line.Contains(searchTerm))
+                    {
+                        string href = "/HTML/" + Path.GetFileName(htmlFile);
+                        string link = $"<a href='{href}'>{Path.GetFileName(htmlFile)} ({lineNumber})</a> <br>";
+                        result += link;
+                    }
+                    lineNumber++;
+                }
+            }
+            return result;
+        }
+        //public string searchOneKeyword(string keyword)
+        //{
+        //    string searchTerm = HtmlEncode(keyword); // replace with your search term
+        //    string directoryPath = @"http://localhost:65079/HTML"; // replace with your directory path
+        //    string dd = @"" + Server.MapPath("HTML");
+        //    string result = "";
+        //    // Get all HTML files in directory and its subdirectories
+        //    IEnumerable<string> htmlFiles = Directory.GetFiles(dd, "*.html", SearchOption.AllDirectories);
+
+        //    // Search through each HTML file and print out the filename and line number where the search term is found
+        //    foreach (string htmlFile in htmlFiles)
+        //    {
+        //       // string nameFile = htmlFile.Split('\\')[htmlFile.Split('\\').Length - 1];
+        //        int lineNumber = 1;
+        //        foreach (string line in File.ReadLines(htmlFile))
+        //        {
+        //            if (line.Contains(searchTerm))
+        //            {
+        //               // result += $"<a href=\"{htmlFile}\"> {htmlFile} </a> ({lineNumber}) <br> \n\r\r";
+        //                string href = "file:///" + htmlFile;
+        //                string link = $"<a href='{href}'>{Path.GetFileName(htmlFile)} ({lineNumber})</a> <br>";
+        //                result += link;
+        //            }
+        //            lineNumber++;
+        //        }
+        //    }
+        //    return result;
+        //}
+        protected void DoSearch(Object sender,EventArgs e)
+        {
+            string[] arr = this.searchTerm.Text.Split(' ');
+            string result = "";
+            foreach(string a in arr)
+            {
+                result += searchOneKeyword(a);
+            }
+         
+            this.resultsSearch.InnerHtml = result;
+        }
+    }
+}
